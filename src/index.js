14b0c5c0d8c7ef0659f7b81e4a85cb44a79d291b
Turdubaevajazgul <jazgulnvk@gmail.com>
2022-10-03 21:52:18 +0300

    feat: add linters

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./redux/index.js";
 import "./i18next";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
