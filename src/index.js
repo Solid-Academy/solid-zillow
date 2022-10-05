6ad6e1d417e5b3b4b96633e9240f7fb55edab1ae
Turdubaevajazgul <jazgulnvk@gmail.com>
2022-10-06 00:34:40 +0300

    feat: add store and fix

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18next";
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import './firebase'
+import { Provider } from "react-redux";
+import { store } from "./redux/index.js";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Provider store={store}>
         <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
 const rootReducer = combineReducers({});
 export const store = configureStore({
-    reducer: rootReducer
+  reducer: rootReducer
 });
