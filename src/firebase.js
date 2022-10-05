f79affbdf343f4b5688f46b38b3fefa864289439
Azim-941223 <urmanbetovazim@gmail.com>
2022-10-05 21:23:50 +0600

    added:firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
-const firebaseConfig = {
+const firebaseConfig = firebase.initializeApp({
   apiKey: "AIzaSyAdCxnt7yb5S6xH_wqcG8XPWw5kOoJbvzs",
   authDomain: "solid-zillow-a2db5.firebaseapp.com",
   projectId: "solid-zillow-a2db5",
@@ -9,7 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "450827684675",
   appId: "1:450827684675:web:98b47340752c2d6702bdf6",
   measurementId: "G-M395EY56S9"
-};
+});
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = firebase.firestore();
+
+export default firebase;
