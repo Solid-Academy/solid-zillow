c7c55a911258b0e9f4229f5fed8e2c0ad2df5295
Azim-941223 <urmanbetovazim@gmail.com>
2022-10-06 00:56:43 +0600

    feat:public and private, added: react-redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Header from "./components/header/Header";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { PrivateRoute, PublicRoute } from "./routes";
+import HomePage from "./components/home/HomePage";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes> </Routes>
+      <Routes>
+        <Route exact path="/" element={<PublicRoute Component={HomePage} />} />
+        <Route path="/dashboard" element={<PrivateRoute Component={Dashboard} />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18next";
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import './firebase'
+//import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
