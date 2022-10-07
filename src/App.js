866f69a1c0c54b16f9c34799e0c431e4e947e3ef
Aminhan <aminhansss2222@gmail.com>
2022-10-07 16:43:58 +0600

    feat/bugfix eslint and pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
-import Header from "./components/header/Header";
 import { Route, Routes } from "react-router-dom";
 import { PrivateRoute, PublicRoute } from "./routes";
-import HomePage from "./components/home/HomePage";
 import Dashboard from "./components/dashboard/Dashboard";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Routes>
-        <Route exact path="/" element={<PublicRoute Component={HomePage} />} />
-        <Route path="/dashboard" element={<PrivateRoute Component={Dashboard} />} />
+        <Route exact path="/" element={<PublicRoute Component={<HomePage />} />} />
+        <Route path="/dashboard" element={<PrivateRoute Component={<Dashboard />} />} />
       </Routes>
     </div>
   );
