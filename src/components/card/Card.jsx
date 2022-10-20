b6f13920132129ae559dd61faf76916fb51f6b02
Turdubaevajazgul <jazgulnvk@gmail.com>
2022-10-20 21:45:13 +0300

    feat: delete links and change contain

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,22 +8,24 @@ function Card() {
   const { t } = useTranslation();
   return (
     <>
-      <div className={css.container}>
-        <div className={css.CardPage}>
-          <a href="" className={css.buyhome}>
-            {CardList.map((element) => (
-              <div className={css.card} key={element.id}>
-                <div>
-                  <img className={css.homeImg} src={element.img} alt="" />
+      <div className={css.background}>
+        <div className={css.container}>
+          <div className={css.CardPage}>
+            <a href="" className={css.buyhome}>
+              {CardList.map((element) => (
+                <div className={css.card} key={element.id}>
+                  <div>
+                    <img className={css.homeImg} src={element.img} alt="" />
+                  </div>
+                  <div className={css.cardText}>
+                    <h1 className={css.title}>{t("card." + element.title)}</h1>
+                    <p className={css.text}>{t("card." + element.text)}</p>
+                    <button className={css.button}>{t("card." + element.btn)}</button>
+                  </div>
                 </div>
-                <div className={css.cardText}>
-                  <h1 className={css.title}>{t("card." + element.title)}</h1>
-                  <p className={css.text}>{t("card." + element.text)}</p>
-                  <button className={css.button}>{t("card." + element.btn)}</button>
-                </div>
-              </div>
-            ))}
-          </a>
+              ))}
+            </a>
+          </div>
         </div>
       </div>
     </>
--- src/components/card/CardLinks.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function CardLink({ id }) {
-  return (
-    <>
-      <Link to="buy">{id}</Link>
-    </>
-  );
-}
