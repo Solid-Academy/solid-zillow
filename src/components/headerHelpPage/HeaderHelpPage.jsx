07e15ef77c05ad10778ecaad69ccbf7355cc6f4b
Amas9nine <amasdina2@gmail.com>
2022-11-01 13:48:44 +0600

    feat:i 18next and <burger/> being in one component

--- src/components/headerHelpPage/HeaderHelpPage.jsx
+++ src/components/headerHelpPage/HeaderHelpPage.jsx
@@ -1,13 +1,27 @@
-import Burger from "./Burger";
 import scss from "./headerHelpPage.module.scss";
-import NavLinks from "./NavLinks";
+import { useState } from "react";
+import Modal from "./Modal";
+import { useTranslation } from "react-i18next";
 
 function HeaderHelpPage() {
+  const { t } = useTranslation();
+  const [isModelOpen, setModalOpen] = useState(false);
+  const onDropDown = () => {
+    setModalOpen(!isModelOpen);
+  };
   return (
     <header className={scss.header}>
       <img className={scss.img} src="/images/headerHelp/Zillow.png" alt="" />
-      <Burger />
-      <NavLinks />
+      <div className={scss.headerSquare}>
+        <button className={scss.burger} onClick={onDropDown}>
+          <img className={scss.imgButton} src="/images/headerHelp/burger.webp" alt="" />
+        </button>
+        {isModelOpen ? <Modal /> : null}
+      </div>
+      <div className={scss.submitBlock}>
+        <a href="#">{t("headerHelper.Submit a request")}</a>
+        <a href="#">{t("headerHelper.Sign in")}</a>
+      </div>
     </header>
   );
 }
--- src/components/headerHelpPage/Modal.jsx
+++ src/components/headerHelpPage/Modal.jsx
@@ -1,11 +1,13 @@
 import scss from "./headerHelpPage.module.scss";
+import { useTranslation } from "react-i18next";
 
 function Modal() {
+  const { t } = useTranslation();
   return (
     <div className={scss.wrapper}>
       <hr />
       <br />
-      <a href="#">Submit a request</a>
+      <a href="#">{t("headerHelper.Submit a request")}</a>
     </div>
   );
 }
