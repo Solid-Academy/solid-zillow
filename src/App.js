c8165ad7506feb2862b0aeaeca13adb2758ebf78
Aminhan <aminhansss2222@gmail.com>
2022-11-05 16:29:52 +0600

    feat/finish Help footer and add i18n

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { PrivateRoute, PublicRoute } from "./routes";
 import Dashboard from "./components/dashboard/Dashboard";
 import HomePage from "./pages/HomePage";
+import HelpFooter from "./pages/helpPage/helpFooter/HelpFooter";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<PublicRoute Component={HomePage} />} />
+        <Route exact path="/help_page" element={<PublicRoute Component={HelpFooter} />} />
         <Route path="/dashboard" element={<PrivateRoute Component={Dashboard} />} />
       </Routes>
     </div>
---
+++ src/pages/helpPage/helpFooter/HelpFooter.jsx
@@ -0,0 +1,30 @@
+import css from "./HelpFooter.module.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+export default function HelpFooter() {
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={css.container}>
+      <div className={css.footer__content}>
+        <Link to="#">{t("HelpFooter.HelpCenter")}</Link>
+        <div className={css.footer_language_selector}>
+          <div className={css.dropdown}>
+            <button onClick={() => setOpen(!open)}>
+              {t("HelpFooter.Language")}{" "}
+              <img src="./images/footer/icons8-chevron-down-24.png" alt="" />
+            </button>
+            {open && (
+              <span className={css.dropdown_menu}>
+                <button>{t("HelpFooter.LanguageSpanish")}</button>
+                <button>{t("HelpFooter.LanguageChines")}</button>
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
