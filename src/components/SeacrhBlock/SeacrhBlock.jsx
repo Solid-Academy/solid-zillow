6a84f2d11329ee811ac70f7fa7f3112ba532a88a
Aminhan <aminhansss2222@gmail.com>
2022-11-10 23:10:40 +0600

    feat: finish seacrhlock

--- src/components/SeacrhBlock/SeacrhBlock.jsx
+++ src/components/SeacrhBlock/SeacrhBlock.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import scss from "./SearchBlock.module.scss";
 
 export default function SearchBlock() {
+  const [openActive, setOpenActive] = useState(false);
+  const { t } = useTranslation();
   return (
-    <div>
-      <div> </div>
-    </div>
+    <>
+      <div className={scss.container}>
+        <div className={scss.searchBlock__content}>
+          <h1>{t("searchBlock.FindIt")}</h1>
+          <div className={scss.inputs__content}>
+            <input
+              onClick={(e) => {
+                setOpenActive(true);
+                e.stopPropagation();
+              }}
+              type="text"
+              placeholder={t("searchBlock.EnterAnAddress")}
+            />
+            {openActive && (
+              <div className={scss.currentLocation__wrapper} onClick={(e) => e.stopPropagation()}>
+                <p>{t("searchBlock.CurrentLocation")}</p>
+              </div>
+            )}
+            <div
+              onClick={() => setOpenActive(false)}
+              className={scss.overlay + " " + (openActive ? scss.active : "")}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,13 @@ import Card from "../components/card/Card";
 import HomeSlider from "../components/carousel/HomeSlider";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import SearchBlock from "../components/SeacrhBlock/SeacrhBlock";
 
 function HomePage() {
   return (
     <div>
       <Header />
+      <SearchBlock />
       <HomeSlider />
       <Card />
       <AboveFooter />
