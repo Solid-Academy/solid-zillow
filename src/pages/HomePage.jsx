03c849b54293cf117fa614118436520af6d0a341
aiskairr <aminhansss2222@gmail.com>
2022-12-10 16:07:49 +0600

    feat/sloid zillow

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,10 @@
 import AboveFooter from "../components/AboveFooter/AboveFooter";
 import Card from "../components/card/Card";
-import HomeSlider from "../components/carousel/HomeSlider";
 import Footer from "../components/footer/Footer";
-import HeaderHelpPage from "./helpPage/headerHelpPage/HeaderHelpPage";
 
 function HomePage() {
   return (
     <div>
-      <HeaderHelpPage />
-      <HomeSlider />
       <Card />
       <AboveFooter />
       <Footer />
