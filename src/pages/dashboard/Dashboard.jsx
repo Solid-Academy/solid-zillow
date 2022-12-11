cd2726ea33b1e6430f3d2e9cd77a8a7c8381d223
Amas9nine <amasdina2@gmail.com>
2022-12-11 13:59:43 +0600

    Redux + state + slider

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../../components/card/Card";
+
+function Dashboard() {
+  return (
+    <>
+      <p>Dashboard</p>
+      <Card />
+    </>
+  );
+}
+
+export default Dashboard;
